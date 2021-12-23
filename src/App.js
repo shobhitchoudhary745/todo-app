d3598127f0d13479bc1f15352608f6ee1fbde2f2
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2021-12-23 13:09:14 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Login from './routers/login'
 import Signup from './routers/signup'
 import Forgot from './routers/forgot'
 import Main from './routers/main'
 function App(){
     return(
-      <BrowserRouter>
+      <HashRouter>
       <Routes>
           <Route exact path="/" element={<Login />} />
           <Route exact path="/signup" element={<Signup />} />
           <Route exact path="/forgot" element={<Forgot />}/>
           <Route exact path="/main" element={<Main />}/>
       </Routes>
-    </BrowserRouter> 
+    </HashRouter> 
     )
 }
 
