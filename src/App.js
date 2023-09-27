5fa595d0185d31103cd2b39c72044e20214087c2
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2023-09-27 17:22:45 +0530

    Last commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter, Route,Switch } from 'react-router-dom'
+import { HashRouter, Route,Routes } from 'react-router-dom'
 import Login from './routers/login'
 import Signup from './routers/signup'
 import Forgot from './routers/forgot'
@@ -7,12 +7,12 @@ import Main from './routers/main'
 function App(){
     return(
       <HashRouter>
-        <Switch>
+        <Routes>
           <Route exact path="/" element={<Login />} />
           <Route exact path="/signup" element={<Signup />} />
           <Route exact path="/forgot" element={<Forgot />}/>
           <Route exact path="/main" element={<Main />}/>
-        </Switch>
+        </Routes>
     </HashRouter> 
     )
 }
